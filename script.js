6feefd4bbddf50692fbd9d7395fb3ee592a5a6b5
Matheus <dacachmatheus@gmail.com>
2020-01-17 14:50:28 -0300

    add constant sizing

--- script.js
+++ script.js
@@ -1,50 +1,67 @@
 const container = document.getElementById('container');
 
-let numRows = 24;
-container.style.gridTemplateRows = `repeat(${numRows}, 1fr)`;
-container.style.gridTemplateColumns = `repeat(${numRows}, 1fr)`; 
-
-for (let col = 1; col <= numRows; col++){
-    for (let row = 1; row <= numRows; row++){
-        const div = document.createElement('div');
-        div.classList.add('cell');
-        div.style.cssText = `width: ${480/numRows}px`;
-        div.style.cssText = `height: ${480/numRows}px`;
-        container.appendChild(div);
-    }
-}
-
 const cells = document.querySelectorAll(".cell");
 
 // add the checking functions to all cells
 
-for (let i = 0; i < cells.length; i++){
-    cell = cells[i];
-    cell.addEventListener('mouseover', function (e) {
-        if (e.target.classList.contains('active')) return; 
-       e.target.classList.add('active');
-        let rgb = generateRandomRGB();
-        e.target.style.backgroundColor = rgb;
-    });
-    cell.addEventListener('mouseout', function (e) {
-        // e.target.classList.remove('active');
+function setupEventListeners() {
+    const cells = document.querySelectorAll(".cell");    
 
-    });
+    for (let i = 0; i < cells.length; i++){
+        cell = cells[i];
+        cell.addEventListener('mouseover', function (e) {
+            if (e.target.classList.contains('active')) return; 
+           e.target.classList.add('active');
+            let rgb = generateRandomRGB();
+            e.target.style.backgroundColor = rgb;
+        });
+        cell.addEventListener('mouseout', function (e) {
+            // e.target.classList.remove('active');
+
+        });
+    }
 }
 
 clearBtn = document.getElementById('clear');
 clearBtn.addEventListener('click', generateNewGrid);
 
+function removeCells () {
+    let first = container.firstElementChild;
+    console.log(first);
+    while (first) {
+        first.remove();
+        first = container.firstElementChild;
+
+    }
+
+
+
+}
+
 function generateNewGrid () {
+    removeCells();
+    clearCells();
+
     newGridSize = prompt("new grid size: ");
 
     container.style.gridTemplateRows = `repeat(${newGridSize}, 1fr)`;
     container.style.gridTemplateColumns = `repeat(${newGridSize}, 1fr)`; 
 
-    clearCells();
+    for (let col = 1; col <= newGridSize; col++){
+        for (let row = 1; row <= newGridSize; row++){
+            const div = document.createElement('div');
+            div.classList.add('cell');
+            container.appendChild(div);
+        }
+    }
+
+    setupEventListeners();
 }
 
 function clearCells () {
+
+    let cells = document.querySelectorAll(".cell");
+
     for (let i = 0; i < cells.length; i++) {
         cells[i].classList.remove('active');
         cells[i].style.backgroundColor = 'white';
@@ -52,7 +69,6 @@ function clearCells () {
 }
 
 // generate a new color for every hover
-
 
 function generateRandomRGB () {
     let r = Math.random()*256;
@@ -64,7 +80,7 @@ function generateRandomRGB () {
 
 
 
-
+generateNewGrid();
 
 
 
